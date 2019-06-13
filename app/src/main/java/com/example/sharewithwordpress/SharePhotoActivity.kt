8e138e20612973a22d112966921f89e515fde501
whitingpt <pete@whitings.org>
2019-06-13 17:11:32 -0600

    restructured shortcode language

--- app/src/main/java/com/example/sharewithwordpress/SharePhotoActivity.kt
+++ app/src/main/java/com/example/sharewithwordpress/SharePhotoActivity.kt
@@ -6,37 +6,39 @@ import android.os.Bundle
 import android.os.StrictMode
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import kotlinx.coroutines.*
 import org.jsoup.Jsoup
 
-class SharePhotoActivity : AppCompatActivity() {
+class SharePhotoActivity : AppCompatActivity(),CoroutineScope by MainScope() {
 
-    data class Shortcode (val type: String,val link: String) {
-        fun build(url: String): String = """[embed-google-$type link="$link" url="$url"]"""
+    data class Shortcode (val type: String,val src: String) {
+        fun build(url: String) = """[embed-google-$type src="$src" click="$url"]"""
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        buildShortcode()
+    }
+
+    private fun buildShortcode () = launch {
         if (intent?.action == INTENT_TRIGGER) {
             val myClipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager?
-            val photoURL = intent.clipData?.getItemAt(0)?.coerceToText(this).toString() ?: return
+            val photoURL = intent.clipData?.getItemAt(0)?.coerceToText(this@SharePhotoActivity)?.toString()?: return@launch
             if (photoURL.startsWith(PHOTO_PAGE_URL, true)) {
-                StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().permitAll().build())
-                val page = Jsoup.connect(photoURL.toString()).get()
+                val page = withContext(Dispatchers.IO){Jsoup.connect(photoURL).get()}
                 val shortcode = when (page.selectFirst(GOOGLE_CONTENT_TYPE).attr(CONTENT_ATTR).toString()) {
                     VIDEO -> Shortcode("video",page.selectFirst(CSS_VIDEO).attr(CONTENT_ATTR).toString())
                     PHOTO -> Shortcode("photo",page.selectFirst(CSS_PHOTO).attr(CONTENT_ATTR).toString().substringBefore("="))
                     ALBUM -> Shortcode("album",photoURL)
-                    else -> return
+                    else -> return@launch
                 }
                 myClipboard?.primaryClip = ClipData.newPlainText(":text", shortcode.build(photoURL))
                 startActivity(packageManager.getLaunchIntentForPackage(WORDPRESS))
 
-            } else Toast.makeText(this, "can't parse $photoURL", Toast.LENGTH_SHORT).show()
+            } else Toast.makeText(this@SharePhotoActivity, "can't parse $photoURL", Toast.LENGTH_SHORT).show()
         }
         finish()
     }
-
-    //    """<div><video controls><source src=$url type="video/mp4"></video></div>"""
 
     companion object {
         const val CONTENT_ATTR = "content"
