de298af45f77b6fbd6db7e01e297010b5d632793
whitingpt <pete@whitings.org>
2019-06-13 09:25:50 -0600

    minor cleanup

--- app/src/main/java/com/example/sharewithwordpress/SharePhotoActivity.kt
+++ app/src/main/java/com/example/sharewithwordpress/SharePhotoActivity.kt
@@ -10,50 +10,46 @@ import org.jsoup.Jsoup
 
 class SharePhotoActivity : AppCompatActivity() {
 
+// should return below be replaced with finish()?
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (intent?.action == "android.intent.action.SEND") {
+        if (intent?.action == INTENT_TRIGGER) {
             val myClipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager?
             val photoURL = intent.clipData?.getItemAt(0)?.coerceToText(this) ?: return
             if (photoPageUrlRegex.matches(photoURL)) {
                 StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().permitAll().build())
                 val page = Jsoup.connect(photoURL.toString()).get()
-                var embedString: String? = null
-                when (page.selectFirst(GOOGLE_CONTENT_TYPE).attr(CONTENT).toString()) {
-                    "video" ->
-                        embedString=makeVideoEmbed(page.selectFirst(CSS_VIDEO).attr(CONTENT).toString())
-                    "google_photos:single_photo" ->
-                        embedString=makePhotoEmbed(page.selectFirst(CSS_PHOTO).attr(CONTENT).toString())
-                    "google_photos:photo_album" ->
-                        embedString=makeAlbumEmbed(photoURL.toString())
+                val embedString = when (page.selectFirst(GOOGLE_CONTENT_TYPE).attr(CONTENT_ATTR).toString()) {
+                    "video" -> embedVideo(page.selectFirst(CSS_VIDEO).attr(CONTENT_ATTR).toString())
+                    "google_photos:single_photo" -> embedPhoto(page.selectFirst(CSS_PHOTO).attr(CONTENT_ATTR).toString())
+                    "google_photos:photo_album" -> embedAlbum(photoURL.toString())
+                    else -> return
                 }
-                if(embedString != null) {
-                    myClipboard?.primaryClip = ClipData.newPlainText(":text", embedString)
-                    startActivity(packageManager.getLaunchIntentForPackage(WORDPRESS))
-                }
-            } else {
-                Toast.makeText(this, "can't parse $photoURL", Toast.LENGTH_SHORT).show()
-            }
+                myClipboard?.primaryClip = ClipData.newPlainText(":text", embedString)
+                startActivity(packageManager.getLaunchIntentForPackage(WORDPRESS))
+
+            } else Toast.makeText(this, "can't parse $photoURL", Toast.LENGTH_SHORT).show()
         }
         finish()
     }
 
-    private fun makeAlbumEmbed(url: String) = """[embed-google-photos-album link="$url"]"""
-    private fun makePhotoEmbed(url: String): String {
+    private fun embedAlbum(url: String) = """[embed-google-photos-album link="$url"]"""
+    private fun embedVideo(url: String) = """<div><video controls><source src=$url type="video/mp4"></video></div>"""
+    private fun embedPhoto(url: String): String {
         val (lessBigURL) = photoUrlRegex.find(url)!!.destructured
         return """[img src="$lessBigURL"]"""
     }
-    private fun makeVideoEmbed(url: String) = """<div><video controls><source src=$url type="video/mp4"></video></div>"""
 
     companion object {
-        val CONTENT = "content"
+        val CONTENT_ATTR = "content"
         val GOOGLE_CONTENT_TYPE = """meta[property=og:type]"""
         val CSS_VIDEO = """meta[property=og:video][content^=https://lh3]"""
         val CSS_PHOTO = """meta[property=og:image][content^=https://lh3]"""
         val WORDPRESS = "org.wordpress.android"
         val photoPageUrlRegex = "https://photos.app.goo.gl/.*".toRegex()
         val photoUrlRegex = "(https://[^=]*)".toRegex()
+        val INTENT_TRIGGER =  "android.intent.action.SEND"
     }
 }
 
